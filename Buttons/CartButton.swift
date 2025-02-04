2c270b0bf52365459b346366a91ea8ab325fbb12
Syskey SK <139443204+PRATIKK0709@users.noreply.github.com>
2025-02-04 14:21:01 +0530

    Create CartButton.swift

---
+++ Buttons/CartButton.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+    struct CartButton: View {
+       var body: some View {
+           Button(action: {}) {
+               Image(systemName: "cart.fill")
+                   .foregroundColor(.black)
+           }
+       }
+    }
